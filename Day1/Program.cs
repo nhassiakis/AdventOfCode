9ec9ce12e0de492e0e5d9b7c06fa328757119f86
Nicolas Hassiakis <40218955+nhassiakis@users.noreply.github.com>
2019-12-03 19:26:52 +0100

    Day 1 finished

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day1
 {
@@ -6,7 +7,34 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> fuelMass = Reader.ReadFile();
+            CalcFuel(fuelMass);
         }
+
+        public static void CalcFuel(List<int> fuelMass)
+        {
+            int total = 0;
+            foreach (var item in fuelMass)
+            {
+                int currentMass = item;
+                double floorIt = currentMass / 3;
+                var thisMassFuel = Convert.ToInt32(Math.Floor(floorIt)) - 2;
+                total += thisMassFuel;
+                currentMass = thisMassFuel;
+                while (thisMassFuel > 0)
+                {
+                    floorIt = currentMass / 3;
+                    thisMassFuel = Convert.ToInt32(Math.Floor(floorIt)) - 2;
+                    if (thisMassFuel > 0)
+                    {
+
+                        currentMass = thisMassFuel;
+                        total += thisMassFuel;
+                    }
+                }
+            }
+            Console.WriteLine(total);
+        }
+
     }
 }
---
+++ Day1/Reader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+namespace Day1
+{
+    public class Reader
+    {
+        public Reader()
+        {
+
+        }
+
+        public static List<int> ReadFile()
+        {
+            string path = @"C:\Users\Nico\source\repos\AdventOfCode\Day1\Data\TextFile1.txt";
+            List<int> fuel = new List<int>();
+            using (StreamReader reader = new StreamReader(path))
+            {
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    int intLine = Convert.ToInt32(line);
+                    fuel.Add(intLine);   // Add to list.
+                }
+
+
+                return fuel;
+            }
+        }
+    }
+
+}
