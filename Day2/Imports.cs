cb785e57d9a85340a6e53b8a9768ea68de42f578
Nicolas Hassiakis <40218955+nhassiakis@users.noreply.github.com>
2019-12-03 22:26:17 +0100

    Added solution Day2, added method to read csv

---
+++ Day2/Imports.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CsvHelper;
+
+
+namespace Day2
+{
+    public static class Imports
+    {
+        public static List<int> ImportCsv()
+        {
+            List<int> allValues = new List<int>();
+            using (StreamReader reader = new StreamReader(@"C:\Users\Nico\source\repos\AdventOfCode\Day2\Data\data.csv"))
+            {
+                string line = null;
+                while (null != (line = reader.ReadLine()))
+                {
+                    string[] values = line.Split(',');
+                    //int[] numbers = new int[values.Length - 1];
+                    for (int i = 0; i < values.Length; i++)
+                        allValues.Add(Int32.Parse(values[i]));
+
+                }
+            }
+            return allValues;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> values = Imports.ImportCsv();
+            RestoreGravityAssist(values);   
+        }
+
+
+        public static void RestoreGravityAssist(List<int> values)
+        {
+            for (int i = 0; i < values.Count; i++)
+            {
+                if (values[i] == 0)
+                {
+
+                }
+            }
+        }
+    }
+}
