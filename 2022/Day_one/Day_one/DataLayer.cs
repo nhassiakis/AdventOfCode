e1448e28dccf3d581168f5070be5241ac8dd220d
nhassiakis <nicolas.hassiakis@hotmail.com>
2022-12-02 00:22:02 +0100

    Added folder for 2022 and complete solutions for day one!

---
+++ 2022/Day_one/Day_one/DataLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_one
+{
+    internal class DataLayer
+    {
+        public List<string> Data { get; set; }
+     
+        
+        
+        
+        public string[] ReadData()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"../../../../PartOneData.txt");
+
+            return lines;
+        }
+    }
+
+}
---
+++ 2022/Day_one/Day_one/Program.cs
@@ -0,0 +1,25 @@
+﻿using Day_one;
+
+string[] input = new DataLayer().ReadData();
+int sum = 0;
+List<int> results = new List<int>();
+for (int i = 0; i < input.Length; i++)
+{
+    string current = input[i];
+
+	if (int.TryParse(current, out int result))
+	{
+		sum += Int32.Parse(current);
+	}
+	else
+	{
+		results.Add(sum);
+		sum = 0;
+	}
+}
+
+//int resultMostCalories = results.Max(maxValue => maxValue);
+//Console.WriteLine(resultMostCalories);
+int[] resultMostCalories = results.OrderByDescending(mostCalories => mostCalories).Take(3).ToArray();
+int topThreeElves = resultMostCalories.Sum();
+Console.WriteLine(topThreeElves);
